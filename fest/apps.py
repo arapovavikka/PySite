911741a5e4449799166fe981d6fa8b79f5e98408
vikka <arapova.vikka@yandex.ru>
2018-12-08 21:10:45 +0300

    ResetAll

--- fest/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FestConfig(AppConfig):
-    name = 'fest'
--- myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\тоша\documents\учеба\магистратура\1 семестр\site\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
