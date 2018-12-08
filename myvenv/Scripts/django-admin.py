41a04e95d38308eb984493356279a54f32b2781c
vikka <arapova.vikka@yandex.ru>
2018-12-08 20:50:34 +0300

    Commit 1

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\тоша\documents\учеба\магистратура\1 семестр\site\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
